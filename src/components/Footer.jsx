b324819edc5dc3400940f01bed9356a0d56b4a93
Shubham Tiwari <shubh.tiwari2244@gmail.com>
2026-07-29 11:56:07 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,8 @@
-import { GITHUB_USER_PROFILE } from '../constants/gameConfig';
-
 export default function Footer() {
   return (
     <footer className="w-full border-t border-white/10 bg-black/25 py-3 text-center text-sm text-slate-100 backdrop-blur-md">
       Game created by{' '}
-      <a className="font-semibold text-indigo-300 underline hover:text-indigo-200" title="Follow me on GitHub!" href={GITHUB_USER_PROFILE} target="_blank" rel="noreferrer">
+      <a className="font-semibold text-indigo-300 underline hover:text-indigo-200" title="Follow me on GitHub!" href={shubham18024.github.io} target="_blank" rel="noreferrer">
         Shubham!
       </a>
       , made with ❤️
